0886560b99e1c12b5f696ad06ebd4750198ec0e5
Riki Fridrich <riki@fczbkk.com>
2014-05-04 10:00:01 +0200

    yeoman

---
+++ yeoman/init.sh
@@ -0,0 +1,14 @@
+# Install Yeoman
+npm install -g yo
+
+# Install generator
+npm install -g generator-webapp
+
+# Check installed generators
+yo -h
+
+# Scaffold new project
+yo webapp
+
+# Run livereload
+grunt serve
